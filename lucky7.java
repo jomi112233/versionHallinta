c6aac67c0864a18cf420ecc834a118682018dc26
jomi <76823844+jomi112233@users.noreply.github.com>
2024-10-24 17:11:12 +0300

    Vaihe 2 lucky-7 tehtävään raha toimii nyt ja jokaisesta voitosta saa 2€ ja yksi peli maksaa 1€

--- lucky7.java
+++ lucky7.java
@@ -7,21 +7,29 @@ public class lucky7 {
         Random rand  = new Random();
 
         boolean gaming = true;
+
+        int raha = 5;
         
 
 
         while(gaming == true){
+            System.out.println("Money: " + raha + "€");
+
+            raha = raha - 1;
+
             for(int i = 0 ; i <= 2 ; i++){
             int rand1 = rand.nextInt(10);
             
             if(rand1 == 7){
                 System.out.println("You Won");
+                raha = raha + 2;
             }
             else{
                 System.out.println(rand1);
+
             }
-            
             }
+            System.out.println("End money:" + raha + "€");
         break;
         }
     }
